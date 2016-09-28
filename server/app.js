384d9a4aa7ab44687f5c5658c1c848ee3bd4f4ea
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-09-28 16:59:44 -0500

    getThings route

--- server/app.js
+++ server/app.js
@@ -5,23 +5,69 @@ var bodyParser = require('body-parser');
 var urlencodedParser =  bodyParser.urlencoded({extended: false});
 var port = process.env.PORT || 8080;
 
-var mongoose = require('mongoose');
-app.use(bodyParser.json);
-
-// require shelfModelfor mongodb
-var shelfModel = require('../models/shelfModel.js')
-
-// connect to the db with db name = shelf
-mongoose.connect('mongodb://localhost:27017/shelf');
-
 // spin up server
 app.listen(port, function(){
   console.log('listening on port:', port);
 });
 
+var mongoose = require('mongoose');
+app.use(bodyParser.json());
+
+// require shelfModelfor mongodb
+var shelfModel = require('../models/shelfModel.js');
+
+// connect to the db with db name = shelf
+mongoose.connect('mongodb://localhost:27017/shelf');
+
+
+
 app.get('/', function(req, res){
   console.log('base url hit');
   res.sendFile(path.resolve('public/views/index.html'));
 });
+
+////////////////////////////// get all thigns////////////
+// show all things
+app.get('/getThings', function(req, res){
+  console.log('in getThings');
+  shelfModel.find({}, function(err, thingsResults){
+    if(err){
+      console.log('error occured:', err);
+      res.sendStatus(500);
+    }else{
+      console.log('thingsResults:', thingsResults);
+      res.send(thingsResults);
+    }
+  });
+});
+
+
+
+app.get('/test', function(req, res) {
+  console.log('in test');
+
+  var justin = new shelfModel({
+    object_name: 'sega',
+    description: 'handheld sega',
+    owner: 'Justin',
+    img_url: 'http://retrothing.typepad.com/photos/uncategorized/seganomad.jpg'
+    });
+
+    justin.save(function(err) {
+      if(err){
+        console.log(err);
+        res.sendStatus(500); // nope!
+      }else{
+        console.log('justin saved!');
+        res.sendStatus(201); // 201 - created
+      }
+    });
+});
+
+
+
+
+
+
 
 app.use(express.static('public'));
