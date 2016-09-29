c2b0916ba6c3fefb48973ddce9635e141b3feb49
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-29 10:58:48 -0500

    add item from form works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,40 +6,84 @@ var lock = new Auth0Lock( 'le4glthQI5NgyhNzP99mKZJjzsr1I6QP', 'thatspaceguy.auth
 // log out url, from Auth0
 var logOutUrl = 'https://thatspaceguy.auth0.com/v2/logout';
 
-myApp.controller('displayController', ['$scope', 'ShelfFactory', function($scope, ShelfFactory){
-  $scope.loggedIn = false;
+myApp.controller('displayController', ['$scope', '$http', 'ShelfFactory', function($scope, $http, ShelfFactory){
+  var fillShelf = function(){
+    ShelfFactory.fillShelf().then(
+      function(){
+        console.log(ShelfFactory.shelf());
+        $scope.shelf = ShelfFactory.shelf();
+    });
+  };
+  //fill shelf on load
+  fillShelf();
+
+  $scope.onLoad = function(){
+    console.log( 'in init' );
+    // check if a user's info is saved in localStorage
+    if( JSON.parse( localStorage.getItem( 'userProfile' ) ) ){
+      // if so, save userProfile as $scope.userProfile
+      $scope.userProfile = JSON.parse( localStorage.getItem( 'userProfile' ) );
+      console.log( 'loggedIn:', $scope.userProfile );
+      $scope.loggedIn = true;
+    }
+    else{
+      // if not, make sure we are logged out and empty
+      emptyLocalStorage();
+      $scope.loggedIn = false;
+    }
+  };
+  $scope.onLoad();
 
   $scope.logIn = function(){
-    $scope.loggedIn = true;
+    console.log( 'in logIn' );
+    lock.show( function( err, profile, token ) {
+      if (err) {
+        console.error( "auth error: ", err);
+      } // end error
+      else {
+        // save token to localStorage
+        localStorage.setItem( 'userToken', token );
+        // save user profile to localStorage
+        localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+        // reload page because dirtyhaxorz
+        location.reload();
+      } // end no error
+    }); //end lock.show
   };
 
   $scope.logOut = function(){
-    $scope.loggedIn = false;
+    $http({
+      method:'GET',
+      url: logOutUrl,
+    }).then( function( data ){
+      // if logged out OK
+      if( data.data == 'OK' ){
+        // empty localStorage
+        emptyLocalStorage();
+        $scope.loggedIn = false;
+      }
+    })
   };
   console.log('ng');
 
   $scope.shelf = [];
-  //fill shelf on load
-  ShelfFactory.fillShelf().then(
-    function(){
-      console.log(ShelfFactory.shelf());
-      $scope.shelf = ShelfFactory.shelf();
-    });
 
-    var putThingOnShelf = function(){
-      var thing = {
-        description: $scope.descriptionIn,
-        owner: undefined, //!!---TAKE FROM AUTH0
-        imageUrl: $scope.imageIn
-      };
-      return ShelfFactory.addToShelf(thing);
+
+  $scope.putThingOnShelf = function(){
+    var thing = {
+      description: $scope.descriptionIn,
+      owner: $scope.userProfile.name,
+      imageUrl: $scope.imageIn
     };
-
-}]);
-
-
-myApp.controller('privelegedController', ['$scope', 'ShelfFactory', function($scope, ShelfFactory){
+    ShelfFactory.addToShelf(thing).then(fillShelf);
+  };
 
 
 
 }]);
+
+
+var emptyLocalStorage = function(){
+  localStorage.removeItem( 'userProfile' );
+  localStorage.removeItem( 'userToken' );
+}; // end emptyLocalStorage
